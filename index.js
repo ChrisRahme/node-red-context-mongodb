ddd90766073e232844816fcd3dba4061f7e568eb
ChrisRahme <chris.rahme@hotmail.com>
2022-06-16 10:57:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -295,8 +295,8 @@ MongoContext.prototype.set = function(scope, key, value, callback) {
             console.error('\n[MONGODB CONTEXT] Failed to set key/value pair in MongoDB Context')
             console.error(err)
             console.error(err2)
-            console.error('----------')
-            console.error(callback)
+            console.error('Here is the callback')
+            console.error(callback.toString())
         }
     }
 }
